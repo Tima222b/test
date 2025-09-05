77e2fae7bf40af9b89c3f3b91972b8a3c5384d86
Timur <yen.tima396@bk.ru>
2025-09-05 17:46:21 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var indexShow;
+var region = document.getElementsByClassName("region");
+
+function regionshow(x) {
+    show(indexShow = x);
+}
+
+function show(x) {   
+    for(var i = 0; i < region.length; i++) {
+        region[i].style.display = "none";
+    }
+
+    region[indexShow].style.display = "block";
+}
+
+function hide() {
+    for(var i = 0; i < region.length; i++) {
+        region[i].style.display = "none";
+    }
+}
